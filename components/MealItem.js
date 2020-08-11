b55d5bbbd0cd1f8759536c7766d3cd23c9e70646
za-n-6 <zara.nasim96@gmail.com>
2020-08-11 22:49:23 +0500

    initiated HeaderButtons by installing a library called react-nativigation-header-buttons

--- components/MealItem.js
+++ components/MealItem.js
@@ -56,6 +56,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f5f5f5",
     borderRadius: 10,
     overflow: "hidden",
+    marginVertical: 10,
   },
   mealRow: {
     flexDirection: "row",
