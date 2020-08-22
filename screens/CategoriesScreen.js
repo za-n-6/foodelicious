bc5afe9916d3e4b8907e18183b2f5ccb4ace7b0e
za-n-6 <zara.nasim96@gmail.com>
2020-08-22 21:37:11 +0500

    setting up Redux and handling State Management ,selecting State slice from reducers through useSelect

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { CATEGORIES } from "../data/dummy-data";
